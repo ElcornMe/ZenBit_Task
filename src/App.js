bb18c2d798f765b18feb8dc8c560ba379238df5f
Denys <elcornme@gmail.com>
2022-07-25 14:33:08 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
     const requestJson = JSON.stringify(requestData);
     console.log(requestJson);
     try {
-      const response = await fetch("https://murmuring-sea-33658.herokuapp.com/", {
+      const response = await fetch("https://murmuring-sea-33658.herokuapp.com/api", {
         method: "POST",
         headers: {
           'Content-Type': 'application/json'
